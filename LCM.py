b3094af70a66b76a4065a3d394c9b85d936c4f7c
21A91A05I1 <97424614+21A91A05I1@users.noreply.github.com>
2022-06-28 22:48:26 +0530

    def lcm(a,b):     c=b     while True:         if c%a==0 and c%b==0:             return c         c+=1 a,b=map(int,input().split()) print(lcm(a,b))

--- LCM.py
+++ LCM.py
@@ -1,9 +1,8 @@
+def lcm(a,b):
+    c=b
+    while True:
+        if c%a==0 and c%b==0:
+            return c
+        c+=1
 a,b=map(int,input().split())
-if a>b:
-    a,b=b,a
-c=b
-while True:
-    if c%a==0 and c%b==0:
-        print(c)
-        break
-    c+=1
+print(lcm(a,b))
