4f3f94b824a05b9446dc63022f0334442fa00204
21A91A05I1 <97424614+21A91A05I1@users.noreply.github.com>
2022-10-12 15:57:29 +0530

    s = input().lower() s = s.split(" ") a = s[0] c = 0 x = '' for i in a:     count = 0     for j in range(1,len(s)):         if i in s[j]:             count+=1     if count==len(s)-1:         x+=i if len(x)==0:     print("-1") else:     print(min(x))

--- Common_characters_-_3.py
+++ Common_characters_-_3.py
@@ -1,22 +1,16 @@
-a=input()
-a=a.lower()
-a=a.split()
-k=""
-l=str(a[0])
-for j in l:
-    c=0
-    for x in range(1,len(a)):
-        a[x]=str(a[x])
-        if j in a[x]:
-            c+=1
-    if c==len(a)-1:
-        k+=j
-if len(k)==0:
-    print(-1)
+s = input().lower()
+s = s.split(" ")
+a = s[0]
+c = 0
+x = ''
+for i in a:
+    count = 0
+    for j in range(1,len(s)):
+        if i in s[j]:
+            count+=1
+    if count==len(s)-1:
+        x+=i
+if len(x)==0:
+    print("-1")
 else:
-    k=set(k)
-    k=sorted(k)
-    for i in k:
-        print(i)
-        break
-        
+    print(min(x))
